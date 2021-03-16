5765114e5b3eeb479de2f36dcd0ed97894b2e7bf
Andrew Pungprakearti <andrew@alephsf.com>
2021-03-16 14:43:23 -0700

    feat: Add index page

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.home_Title}>Aleph Nought NextJS Boilerplate</h1>
+      <img className={styles.home_Image} src='/ceo.jpeg' />
+    </div>
+  )
+}
