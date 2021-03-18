7080553809e7968148623df6dbf50f06d4a04962
Andrew Pungprakearti <andrew@alephsf.com>
2021-03-18 10:42:18 -0700

    feat: Add stylelint sass linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
 import styles from './index.module.scss'
 
 export default function Home() {
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    if(!loaded) {
+      window.setTimeout(() => {
+        setLoaded(true)
+      }, 500)
+    }
+  }, [])
+
   return (
     <div className={styles.home}>
       <h1 className={styles.home_Title}>Aleph Nought NextJS Boilerplate</h1>
       <img className={styles.home_Image} src='/ceo.jpeg' />
-      <audio src='/sound.wav' autoPlay />
+      {loaded && (
+        <audio src='/sound.wav' autoPlay />
+      )}
     </div>
   )
 }
