9b08523ac73c0771cf0d554bd7cbf48a42bd2954
Andrew Pungprakearti <andrew@alephsf.com>
2021-12-10 11:01:53 -0800

    Create FunctionalComponent and clean up code

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,15 @@
-import { useEffect, useState } from 'react'
 import styles from './index.module.scss'
 
 export default function Home() {
-  const [loaded, setLoaded] = useState(false)
-
-  useEffect(() => {
-    if(!loaded) {
-      window.setTimeout(() => {
-        setLoaded(true)
-      }, 500)
-    }
-  }, [])
 
   return (
-    <div className={styles.home}>
-      <h1 className={styles.home_Title}>Aleph Nought NextJS Boilerplate</h1>
-      <img className={styles.home_Image} src='/ceo.jpeg' />
-      {loaded && (
-        <audio src='/sound.wav' autoPlay />
-      )}
+    <div className={styles.wrap}>
+      <h1>
+        NextJS Boilerplate by Andrew Pungprakearti
+      </h1>
+      <a href='mailto:andrew.pungprakearti@gmail.com'>
+        andrew.pungprakearti@gmail.com
+      </a>
     </div>
   )
 }
