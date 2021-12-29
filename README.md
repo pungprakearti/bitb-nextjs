2bfee74ea4ae9e268e3f9d8762e46a1192895126
pungprakearti <andrew.pungprakearti@gmail.com>
2021-12-29 10:27:04 -0800

    Create PatternLibrary, add typography

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # NextJS Boilerplate  
 
+- TypeScript enabled
+- Pattern library
+- Grid mixins
+
 ## Run application
-`yarn dev`
+1. Install packages: `yarn`
+2. Start server: `yarn dev`
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
+import PatternLibrary from '@/components/PatternLibrary'
+import cx from 'classnames'
 import styles from './index.module.scss'
 
 export default function Home() {
 
   return (
-    <div className={styles.wrap}>
-      <h1>
-        NextJS Boilerplate by Andrew Pungprakearti
-      </h1>
-      <a href='mailto:andrew.pungprakearti@gmail.com'>
-        andrew.pungprakearti@gmail.com
-      </a>
+    <div className={cx(styles.wrap, ' container')}>
+      <span>
+        <h2>
+          NextJS Boilerplate by
+        </h2>
+        <p> <a href='mailto:andrew.pungprakearti@gmail.com'>Andrew Pungprakearti</a></p>
+      </span>
+      <PatternLibrary />
     </div>
   )
 }
