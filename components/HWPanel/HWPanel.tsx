305ecd7357910e9ee493fe28f21c9c134ac920b3
Andrew Pungprakearti <andrew@alephsf.com>
2023-01-09 16:40:25 -0800

    Functionality for Direction and Cell buttons

--- components/HWPanel/HWPanel.tsx
+++ components/HWPanel/HWPanel.tsx
@@ -1,54 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import cx from 'classnames'
 import styles from './HWPanel.module.scss'
 
 type Props = {
   handleMainPower: Function
 }
-// 0 is off, 0.5 is dim, and 1 is on
-type LOpts = 0 | 0.5 | 1
-type ButtonLights = Record<string, LOpts>
+
+type CurDir = 'dirUp' | 'dirLeft' | 'dirLeftDown' | 'dirRight' | ''
 
 const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
-  const initializeButtonLights: ButtonLights = {
-    mainOn: 0.5,
+  const initDirPower = {
     dirUp: 0,
     dirLeft: 0,
     dirLeftDown: 0,
     dirRight: 0,
-    center1: 0,
-    center2: 0,
-    center3: 0,
-    center4: 0,
-    center5: 0,
-    center6: 0,
-    center7: 0,
-    center8: 0,
-    center9: 0,
-    center10: 0,
-    center11: 0,
-    center12: 0,
-    center13: 0,
-    center14: 0,
-    center15: 0,
-    center16: 0,
-    center17: 0,
-    center18: 0,
-    center19: 0,
-    center20: 0,
-    center21: 0,
-    radioOn: 0,
-    radioOff: 0,
   }
 
-  const [buttonLights, setButtonLights] = useState(initializeButtonLights)
+  const initCellPower = [...Array(21).fill(false)]
+
+  const initDirTracker = {
+    dirUp: false,
+    dirLeft: false,
+    dirLeftDown: false,
+    dirRight: false,
+  }
+
+  const [mainPower, setMainPower] = useState(false)
+  const [dirPower, setDirPower] = useState(initDirPower)
+  const [cellPower, setCellPower] = useState(initCellPower)
+  const [dirTracker, setDirTracker] = useState(initDirTracker)
+  const [curDir, setCurDir] = useState<CurDir>('')
+
+  useEffect(() => {
+    // If all cells are off while using a direction, turn off that direction
+    if (!cellPower.includes(true)) {
+      setDirPower({
+        ...dirPower,
+        [curDir]: 0,
+      })
+      setDirTracker({
+        ...dirTracker,
+        [curDir]: true,
+      })
+    }
+  }, [cellPower])
 
   // Main power for all hardware
   const handlePower = (turnOn: boolean) => {
     if (turnOn) {
-      setButtonLights({
-        ...buttonLights,
-        mainOn: 1,
+      setMainPower(true)
+      setDirPower({
         dirUp: 0.5,
         dirLeft: 0.5,
         dirLeftDown: 0.5,
@@ -57,24 +58,83 @@ const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
       return handleMainPower(true)
     }
 
-    setButtonLights(initializeButtonLights)
+    // When powered off, restart everything
+    setMainPower(false)
+    setDirPower(initDirPower)
+    setCellPower(initCellPower)
+    setDirTracker(initDirTracker)
+    setCurDir('')
     return handleMainPower(false)
   }
 
   const handleDirPower = (
     dir: 'dirUp' | 'dirLeft' | 'dirLeftDown' | 'dirRight'
   ) => {
-    // turn on button light and dim other directions
-    setButtonLights({
-      ...buttonLights,
-      dirUp: 0.5,
-      dirLeft: 0.5,
-      dirLeftDown: 0.5,
-      dirRight: 0.5,
+    // Don't allow power to direction buttons if main power is off
+    if (!mainPower) return
+
+    // Do not turn on again if all cells turned off
+    if (dirTracker[dir]) return
+
+    // Track current direction button in state
+    setCurDir(dir)
+
+    // Turn on button light and dim other directions
+    setDirPower({
+      dirUp: dirTracker.dirUp ? 0 : 0.5,
+      dirLeft: dirTracker.dirLeft ? 0 : 0.5,
+      dirLeftDown: dirTracker.dirLeftDown ? 0 : 0.5,
+      dirRight: dirTracker.dirRight ? 0 : 0.5,
       [dir]: 1,
     })
 
-    // randomize center buttons on
+    // Randomize powered cell buttons
+    let randCells: number[] = []
+    while (randCells.length < Math.ceil(Math.random() * 10)) {
+      const randCell = Math.floor(Math.random() * 21)
+      if (!randCells.includes(randCell)) {
+        randCells.push(randCell)
+      }
+    }
+
+    // Assign power to cells
+    let tempCellPower = []
+    for (let cell = 0; cell < cellPower.length; cell++) {
+      if (randCells.includes(cell)) tempCellPower.push(true)
+      else tempCellPower.push(false)
+    }
+    setCellPower(tempCellPower)
+  }
+
+  const handleCellClick = (cell: number) => {
+    // If all cells are off, don't allow any to be turned on
+    if (!cellPower.includes(true)) return
+
+    let tempCellPower = [...cellPower]
+    tempCellPower[cell] = !tempCellPower[cell]
+    setCellPower(tempCellPower)
+  }
+
+  // Create cells element
+  let cellsEl = []
+  for (let cell = 0; cell < cellPower.length; cell++) {
+    if (cellPower[cell]) {
+      cellsEl.push(
+        <button
+          className={styles.on}
+          onClick={() => handleCellClick(cell)}
+          key={cell}
+        >
+          {cell}
+        </button>
+      )
+    } else {
+      cellsEl.push(
+        <button onClick={() => handleCellClick(cell)} key={cell}>
+          {cell}
+        </button>
+      )
+    }
   }
 
   return (
@@ -82,8 +142,8 @@ const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
       <div className={styles.power}>
         <button
           className={cx(
-            { [styles.dim]: buttonLights.mainOn === 0.5 },
-            { [styles.on]: buttonLights.mainOn === 1 }
+            { [styles.dim]: !mainPower },
+            { [styles.on]: mainPower }
           )}
           onClick={() => handlePower(true)}
         >
@@ -94,8 +154,8 @@ const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
       <div className={styles.direction}>
         <button
           className={cx(
-            { [styles.dim]: buttonLights.dirUp === 0.5 },
-            { [styles.on]: buttonLights.dirUp === 1 }
+            { [styles.dim]: dirPower.dirUp === 0.5 },
+            { [styles.on]: dirPower.dirUp === 1 }
           )}
           onClick={() => handleDirPower('dirUp')}
         >
@@ -103,8 +163,8 @@ const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
         </button>
         <button
           className={cx(
-            { [styles.dim]: buttonLights.dirLeft === 0.5 },
-            { [styles.on]: buttonLights.dirLeft === 1 }
+            { [styles.dim]: dirPower.dirLeft === 0.5 },
+            { [styles.on]: dirPower.dirLeft === 1 }
           )}
           onClick={() => handleDirPower('dirLeft')}
         >
@@ -112,8 +172,8 @@ const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
         </button>
         <button
           className={cx(
-            { [styles.dim]: buttonLights.dirLeftDown === 0.5 },
-            { [styles.on]: buttonLights.dirLeftDown === 1 }
+            { [styles.dim]: dirPower.dirLeftDown === 0.5 },
+            { [styles.on]: dirPower.dirLeftDown === 1 }
           )}
           onClick={() => handleDirPower('dirLeftDown')}
         >
@@ -121,37 +181,15 @@ const HWPanel: React.FC<Props> = ({ handleMainPower }) => {
         </button>
         <button
           className={cx(
-            { [styles.dim]: buttonLights.dirRight === 0.5 },
-            { [styles.on]: buttonLights.dirRight === 1 }
+            { [styles.dim]: dirPower.dirRight === 0.5 },
+            { [styles.on]: dirPower.dirRight === 1 }
           )}
           onClick={() => handleDirPower('dirRight')}
         >
           RGHT
         </button>
       </div>
-      <div className={styles.center}>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-        <button>X</button>
-      </div>
+      <div className={styles.center}>{cellsEl}</div>
       <div className={styles.radio}>
         <div className={styles.radioLeft}>
           <button>1</button>
