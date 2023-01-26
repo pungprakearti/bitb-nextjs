76b65613935fd89b91f21aac6e352b9cae5532e3
Andrew Pungprakearti <andrew@alephsf.com>
2023-01-26 15:56:25 -0800

    WIP selector

--- components/HWSelector/HWSelector.tsx
+++ components/HWSelector/HWSelector.tsx
@@ -1,16 +1,75 @@
+import { useState, useEffect } from 'react'
+import { PowerStatus } from 'types'
+import PressableButton from '../PressableButton'
 import styles from './HWSelector.module.scss'
 
-const HWSelector: React.FC = () => (
-  <div className={styles.wrap}>
-    <div className={styles.screen}>Selector 15</div>
-    <div className={styles.bottom}>
-      <button>1</button>
-      <button>0</button>
-      <button>UP</button>
-      <button>DN</button>
-      <button>ENT</button>
-    </div>
-  </div>
-)
+type Props = {
+  energy: number
+  addEnergy: Function
+  mainPowerOn: boolean
+}
 
+const HWSelector: React.FC<Props> = ({ energy, addEnergy, mainPowerOn }) => {
+  const [power, setPower] = useState(false)
+
+  useEffect(() => {
+    if (!mainPowerOn) setPower(false)
+  }, [mainPowerOn])
+
+  const handleClick = (op: 'on' | 'off' | 'UP' | 'DOWN' | 'ENTR') => {
+    switch (op) {
+      case 'on':
+        if (!power && energy > 15) {
+          addEnergy(-15)
+          return setPower(true)
+        } else break
+      case 'off':
+        if (power) {
+          addEnergy(15)
+          return setPower(false)
+        } else break
+      case 'UP':
+        break
+      case 'DOWN':
+        break
+      default:
+        break
+    }
+  }
+
+  let powerStatus: PowerStatus = 'off'
+  if (energy > 15) powerStatus = 'dim'
+  if (power) powerStatus = 'on'
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.screen}>Selector 15</div>
+      <div className={styles.bottom}>
+        <div className={styles.buttonsTop}>
+          <div className={styles.buttonsTopLeft}>
+            <PressableButton
+              handleClick={() => handleClick('on')}
+              text='1'
+              power={powerStatus}
+            />
+            <PressableButton handleClick={() => handleClick('off')} text='0' />
+          </div>
+          <div className={styles.buttonsTopRight}>
+            <PressableButton handleClick={() => handleClick('UP')} text='UP' />
+            <PressableButton
+              handleClick={() => handleClick('DOWN')}
+              text='DOWN'
+            />
+          </div>
+        </div>
+        <div className={styles.buttonsBottom}>
+          <PressableButton
+            handleClick={() => handleClick('ENTR')}
+            text='ENTR'
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
 export default HWSelector
