f51c5196101e2ce6908ad6c81904398c19dad8be
Andrew Pungprakearti <andrew@alephsf.com>
2023-02-04 11:39:46 -0800

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import Script from 'next/script'
 import 'styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-2R96VVPY0P'
+      />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2R96VVPY0P', {
+          page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
